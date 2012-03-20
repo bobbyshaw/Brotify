bc621f4b0f1b18826c9b836cdd96099c8ee04bc5
Chris Moore <cwm322@gmail.com>
2012-03-20 16:55:42 +0000

    errr, cant remmeber, something with artist scores

--- brotify.js
+++ brotify.js
@@ -3,6 +3,7 @@ var models = sp.require('sp://import/scripts/api/models');
 var player = models.player;
 var playCount;
 var apiKey = 'b25b959554ed76058ac220b7b2e0a026';
+var scores = new Array;
 
 exports.init = init;
 
@@ -11,6 +12,7 @@ function init() {
     updatePageWithTrackDetails();
 	playCount = 0;	
 	$('#playCount').text(playCount);
+	getFriends();
     player.observe(models.EVENT.CHANGE, function (e) {
 
         // Only update the page if the track changed
@@ -35,22 +37,18 @@ function updatePageWithTrackDetails() {
     } else {
         var track = playerTrackInfo.data;
         header.innerHTML = track.name + " on the album " + track.album.name + " by " + track.album.artist.name + ".";
-		//getLastFMShouts();
-		getFriends();
     }
 }
 
 function getFriends(){
 	var user = 'cassanova1212';
-	var friends = new Array();
 	getTopArtists(user);
-	friends = $.get('http://ws.audioscrobbler.com/2.0/?method=user.getfriends&user='+user+'&api_key='+apiKey+'&format=json', function(data){
-		console.log(data);
+	$.get('http://ws.audioscrobbler.com/2.0/?method=user.getfriends&user='+user+'&api_key='+apiKey+'&format=json', function(data){
 		jQuery.each(data.friends.user, function(i,val){
-			friends[i] = val.name;
 			getTopArtists(val.name);
-		})
+		});
 	});
+	console.log(scores);
 }
 function getTopArtists(user){
 	var playerTrackInfo = player.track;
@@ -63,13 +61,13 @@ function getTopArtists(user){
 
 function parseTopArtists(data,user){
 	var score = 0;
-	console.log(data);
+	if (data.topartists.total == 0 ) return;
 	jQuery.each(data.topartists.artist, function(i,val){
 		if (val.playcount > 100 ){ score += 50; }
 		else if (val.playcount  > 10) { score += 10; } 
-		else { score+= 1;}
+		else { score += 1;}
     });
-	$('#score').after(user + ': ' +score);
+	$('#score').after(user + ' ' + score);
 	$('#score').after('<br>');
 }
 
